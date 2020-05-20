307da8b5d93b4c34d8a820c06f63c4596cfd4a6e
stephen bennett <stephenbennett@stephens-MacBook-Pro.local>
2020-05-20 17:37:45 -0400

    initial test

--- main.js
+++ main.js
@@ -95,7 +95,9 @@ downloadBtn.addEventListener("click", (event) => {
     })
     .then(() => {
       var form = `<form action="https://codepen.io/pen/define" method="POST" target="_blank">
-    <input type="hidden" name="data" value='{"html":${JSON.stringify(html)}}' />
+    <input type="hidden" name="data" value='{"html":${JSON.stringify(
+      html
+    )},"css_external": "https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.4.6/tailwind.min.css"}' />
     <input type="image" src="http://s.cdpn.io/3/cp-arrow-right.svg" width="40" height="40" value="Create New Pen with Prefilled Data" class="codepen-mover-button">
     <button type="submit">codepen it up!</button>
     </form>`;
